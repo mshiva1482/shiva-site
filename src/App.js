6fa7fbb9edabc1903d14effa2cdf6ebf461a2488
Shiva Gurunath <71997088+mshiva1482@users.noreply.github.com>
2023-10-03 16:57:47 +0530

    test push

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <span>/</span>
         <a href="https://www.linkedin.com/in/mshiva1482/" target="_blank">LinkedIn</a>
         <span>/</span>
-        <a href="https://leetcode.com/kactus/" target="_blank">LeetCode</a>
+        <a href="https://leetcode.com/kactus/" target="_blank">LeetCodas</a>
       </div>
       <SectionV2 imageSrc = {awsCert} header = "AWS Certified Solutions Architect Associate" paragraph = "This certification has equipped me with a comprehensive understanding of designing distributed systems optimizing performance, and ensuring the security and reliability of applications on the AWS platform." width="250px" height="250px" linkName = "badge >" link = "https://www.credly.com/badges/57fa9f5e-82ea-4e70-b9ea-20fe953b2ed7/public_url"/>
       <Section imageSrc = {planet} header = "Exoplanet detection" paragraph = "A machine learning model that predicts whether a celestial object in question is an exoplanet or not." width="250px" height="250px" linkName = "GitHub >" link = "https://github.com/mshiva1482/ExoplanetDetection"/>
